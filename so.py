329a74d179147467c951de5a146fd01735abc46b
analyzehim <raevegnforspam@rambler.ru>
2017-12-12 01:57:34 +0300

    Little Refactor

--- so.py
+++ so.py
@@ -1,124 +1,81 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import codecs
+import time
 
-'''
-a='Привет'
-print a.decode('utf-8')
-'''
-def create_mat():
-    f = open('mat.txt', 'r')
-    for s in f:
-        size=len(s)-1
-        break
-    f.close()
-    print size
-    f = open('mat.txt', 'r')
-    A = [[0 for x in range(size)] for x in range(size)]
-    i=-1
-    j=0
-    for s in f:
-        i+=1
-        j=0
-        if s[len(s)-1]=='\n':
-            for let in s[0:len(s)-1]:
-                A[i][j]=let
-                j+=1
-        else:
-           for let in s:
-                A[i][j]=let
-                j+=1
-    f.close()
-    return A
-def create_set():
-    a=set()
-    f = open('text.txt', 'r')
-    for s in f:
-        #a.add(s[0:len(s)-1])
-        if s[len(s)-1]=='\n':
-            a.add(s[0:len(s)-1])
-        else:
-           a.add(s)
-    f.close()
-    return a
+class WordMatr():
+    def __init__(self, filename):
+        with codecs.open(filename, 'r', encoding='utf8') as f:
+            text = f.read()
+        self.size = len(text.split('\r\n')[0])
+        self.square = [[0 for x in range(self.size)] for x in range(self.size)]
+        for i in range(self.size):
+            for j in range(self.size):
+                self.square[i][j] = text.split('\r\n')[i][j].encode('utf8')
+        return
 
-def print_list(A):
-    s=''
-    for i in range(len(A)):
-             s+= str(A[i])+' '
-    print s[0:len(s)-1]
-def print_mat(A):
-    for i in range(len(A[0])):
-            print_list(A[i])
-def print_set(A):
-    for i in A:
-        print i
+    def __len__(self):
+        return self.size
 
-def not_null(A):
-    for i in range(len(A)):
-        for j in range(len(A)):
-            if A[i][j]!=0:
-                return True
-    return False
-        
+    def __str__(self):
+        output = ''
+        for i in range(self.size):
+            for j in range(self.size):
+                output += self.square[i][j]
+                output += ' '
+            output += '\n'
+        return output
 
-def search(s,set_words,A,B,i,j,size):
-    s+=(A[i][j])
-    if len(s)==size:
-        
-        if s in set_words:
-                print s, j+1,i+1
-          # print_mat(B)
+
+class WordSet():
+    def __init__(self, filename):
+        self.wordset = set()
+        with open(filename, 'r') as f:
+            for word in f:
+                self.wordset.add(word.replace('\n', ''))
+
+
+def search(s, A, B, i, j):
+    s += (A[i][j])
+    if (len(s)/2) == size:
+        if s in set_words.wordset:
+                print s, j+1, i+1
                 return
-                '''
-                if len(s)>7:
-                    global mask
-                    for i in range(len(A)):
-                        for j in range(len(A)):
-                            mask[i][j]=B[i][j]
-                    print_mat(mask)
-                '''
-        
-        return ;
-    B[i][j]=1
-    if (i<len(A)-1) :
-        if  B[i+1][j]==0:
-            search(s,set_words,A,B,i+1,j,size)
-  
-    if (j<len(A)-1):
-        if  B[i][1+j]==0:
-            search(s,set_words,A,B,i,j+1,size)
+        return
 
-    if (i>0):
-        if  B[i-1][j]==0:
-            search(s,set_words,A,B,i-1,j,size)
+    B[i][j] = 1
+    if i < len(A)-1:
+        if B[i+1][j] == 0:
+            search(s, A, B, i+1, j)
+  
+    if j < len(A)-1:
+        if B[i][1+j] == 0:
+            search(s, A, B, i, j+1)
+
+    if i > 0:
+        if B[i-1][j] == 0:
+            search(s, A, B, i-1, j)
            
-    if (j>0):
-        if  B[i][j-1]==0:
-            search(s,set_words,A,B,i,j-1,size)
-    B[i][j]=0
-   
+    if j > 0:
+        if B[i][j-1] == 0:
+            search(s, A, B, i, j-1)
+    B[i][j] = 0
     return
-#mask=[[0 for x in range((7))] for x in range(7)]
+
+
 def go():
-    A = create_mat()
-    print_mat(A)
-    set_words=create_set()
+    A = WordMatr('square.txt')
+    global set_words
+    global size
+    set_words = WordSet('text.txt')
     B = [[0 for x in range(len(A))] for x in range(len(A))]
-    
-    s=''
-    global mask
-    for size in range(5,12):    
+    s = ''
+    for size in range(5, 12):
         for i in range(len(A)):
             for j in range(len(A)):
-                count=1
-                #size1=13-size
-                search(s,set_words,A,B,i,j,size)
-                #print mask
+                search(s, A.square, B, i, j)
 
-    
-
-
-
-
-
-go()
+if __name__ == "__main__":
+    timestamp = time.time()
+    go()
+    print time.time() - timestamp
